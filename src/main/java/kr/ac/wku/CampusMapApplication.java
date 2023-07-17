c327eccd56a552e41a75c90496713c7beb38ffa0
Jeyeon Kim <wpdus2694@naver.com>
2023-07-17 14:47:44 +0900

    Initial Commit

---
+++ src/main/java/kr/ac/wku/CampusMapApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.wku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/wku/CampusMapApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.wku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
